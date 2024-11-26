c1f0c7516db4661a4a1a46d97de61bbd8a995f78
Larabjorn <23-02406@g.batstate-u.edu.ph>
2024-11-26 18:31:53 +0800

    Delete Laboratory Activity 4 - 26 - 11 - 2024 directory

--- Laboratory Activity 4 - 26 - 11 - 2024/Capybara/Capybara.py
+++
@@ -1,9 +0,0 @@
-class Capybara:
-    def __init__(self, name: str, gender: str, age: int):
-        self.name = name
-        self.gender = gender
-        self.age = age
-
-    def __str__(self):
-        gender_full = "Male" if self.gender.upper() == 'M' else "Female"
-        return f"Name: {self.name}, Gender: {gender_full}, Age: {self.age} years old"
